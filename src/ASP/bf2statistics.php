155581d84478b59eb4bc99257d37009e96b43134
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2022-11-01 01:20:58 +0000

    Chore: Bump version to `2.4.1`

--- src/ASP/bf2statistics.php
+++ src/ASP/bf2statistics.php
@@ -23,7 +23,7 @@
 | Define Constants
 | ---------------------------------------------------------------
 */
-    define('CODE_VER', '2.4.0');
+    define('CODE_VER', '2.4.1');
     define('CODE_VER_DATE', '2013-03-12');
 	define('TIME_START', microtime(1));
 	define('DS', DIRECTORY_SEPARATOR);
